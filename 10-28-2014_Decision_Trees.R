d76df42de5802c5f881c53ccc936f157ad1512a2
johnnyquach84 <johnnyquach84@gmail.com>
2014-10-28 15:23:19 -0700

    Update

--- 10-28-2014_Decision_Trees.R
+++ 10-28-2014_Decision_Trees.R
@@ -1,2 +1,33 @@
 #Decision tree used for regression and classification
 #Can be used for both type of analysis
+
+###REGRESSION TREE###
+#Regions, subsetting space into different regions
+#Powerpoint Notes
+#Two Variables
+#X1 = t1  (Root note = X1)
+#Split down middle
+#X1 < t1 go left
+#X1 > t1 go right
+
+#Supervised Learning (nonparametric)
+#understand parametric vs nonparametric
+
+#numerical variables = binary model 
+#Categorical variables = different
+
+#partitions is based on lowest MSE
+#overfit = errors will be high
+
+#tree pruning ??
+#How do we predict the values of Y?
+#splitting criteria is based on minimizing the MSE
+
+###Classification Tree###
+#based on probability
+
+#Pros and Cons of Decision Trees
+
+#Random Forest
+
+
